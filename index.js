ad120107b32e779feaaa5264e7ce7b9d10b796d2
Sanjay Sai <shachsan@hotmail.com>
2018-12-18 11:19:00 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function Scooter(year, color, model){
+  // body...
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+function Driver (name, age, experience) {
+  // body...
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function PickupLocation (address, city) {
+  // body...
+  this.address = address
+  this.city = city
+}
